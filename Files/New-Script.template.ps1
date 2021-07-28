c735178ea08186f9bf3d042865d08a30d1ffa163
John-A-West <brownjohn423@gmail.com>
2021-07-28 11:54:30 -0700

    Modified description for comment-based help

--- Files/New-Script.template.ps1
+++ Files/New-Script.template.ps1
@@ -1,9 +1,10 @@
+#{__requiredmodules__}
 function {__name__} {
     <#
         .SYNOPSIS
             {__description__}
         .DESCRIPTION
-            Detailed description of {__name__}.
+            {__description__}. Details:
         .INPUTS
             None
         .OUTPUTS
@@ -40,3 +41,20 @@ function {__name__} {
 
     }
 }
+<#PSScriptInfo
+.VERSION {__version__}
+.GUID {__guid__}
+.AUTHOR {__author__}
+.COMPANYNAME {__companyname__}
+.DESCRIPTION {__description__}
+.COPYRIGHT
+.TAGS
+.LICENSEURI
+.PROJECTURI
+.ICONURI
+.EXTERNALMODULEDEPENDENCIES
+.REQUIREDSCRIPTS
+.EXTERNALSCRIPTDEPENDENCIES
+.RELEASENOTES
+.PRIVATEDATA
+#>
